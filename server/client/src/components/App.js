e501a791d04a28633cb30b35bcf18eabc223f8fd
Draden Gaffney <dsg2dm@virginia.edu>
2018-08-10 13:05:46 -0700

    basic react

--- server/client/src/components/App.js
+++ server/client/src/components/App.js
@@ -1,25 +1,14 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import { connect } from 'react-redux';
-import * as actions from '../actions';
 
 
-class App extends Component {
-//  componentDidMount() {
-//    this.props.fetchUser();
-//  }
+const App = () => {
+  return (
+    <div>
+      hi there
+    </div>
+  );
+};
 
-  render() {
-    return (
-      <div className="container">
-        <BrowserRouter>
-          <div>
-            <p>Welcome to Aveneu</p>
-          </div>
-        </BrowserRouter>
-      </div>
-    );
-  }
-}
-
-export default connect(null, actions)(App);
+export default App;
