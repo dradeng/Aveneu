b59553218d1f56d4cb2bd6cc869b2c61f6207858
Draden Gaffney <dsg2dm@virginia.edu>
2018-08-11 21:55:54 -0700

    reducer added

--- server/client/src/index.js
+++ server/client/src/index.js
@@ -2,11 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+
 import App from './components/App';
+import reducers from './reducers';
 
 
-
-const store = createStore(() => [], {}, applyMiddleware());
+const store = createStore(reducers, {}, applyMiddleware());
 //Passport Middleware
 //app.use(passport.initialize());
 //app.use(passport.session());
