a53ad2a8cb5af5f1c74f91f35b00466af5312773
Jacob Young <jacob.young@duke.edu>
2018-08-14 21:01:31 +0200

    oops

---
+++ aveneu/client/src/App.js
@@ -0,0 +1,49 @@
+import React from 'react';
+
+import { applyMiddleware, createStore } from 'redux';
+import { Provider } from 'react-redux';
+// import { createLogger } from 'redux-logger';
+import thunk from 'redux-thunk';
+import { PersistGate } from 'redux-persist/lib/integration/react';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import Routes from './Routes';
+import reducers from './reducers';
+
+
+
+/* @TODO: make this applicable to us */
+const persistConfig = {
+    key: 'root',
+    storage: storage,
+    blacklist: [
+        'loggedIn',
+        'appStatus',
+    ],
+};
+
+const persistedReducer = persistReducer(persistConfig, reducers);
+
+let middleware = [thunk];
+
+if ('production' !== process.env.NODE_ENV) {
+    // const logger = createLogger({});
+    // middleware.push(logger);
+}
+
+const store = createStore(persistedReducer, applyMiddleware(...middleware));
+// const store = createStore(persistedReducer, applyMiddleware(thunk));
+
+let persistor = persistStore(store);
+
+function App() {
+    return (
+        <Provider store={store}>
+            <PersistGate loading={null} persistor={persistor}>
+                <Routes />
+            </PersistGate>
+        </Provider>
+    );
+}
+
+export default App;
