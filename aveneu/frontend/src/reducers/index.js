95fd72ada41d225d947711842be125b7c1c054ba
Draden Gaffney <dsg2dm@virginia.edu>
2018-08-24 13:54:28 -0400

    clean mess

--- aveneu/frontend/src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import authReducer from './authReducer';
-import appStatus from './appStatus';
-
-export default combineReducers({
-  auth: authReducer,
-    appStatus: appStatus
-});
